7ae838569ef0c73b0e0caee884d86e7b00560d15
benny <124681388+alocinny@users.noreply.github.com>
2023-09-04 18:30:01 -0300

    converter variavel

---
+++ strings/challenge-2.py
@@ -0,0 +1,16 @@
+'''
+02 - Converte para numero
+
+Escreva um algoritmo que recebe uma string que representa um numero inteiro e retorne o valor convertido para o tipo number.Por exemplo:
+
+------------
+Entrada: "1"
+Saida: 1
+------------
+'''
+
+entrada = input("digite um numero inteiro: ")
+
+saida = int(entrada)
+
+print(saida)
